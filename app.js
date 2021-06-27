764b28f57f6008461c8ba56760588c4815e82fbc
zakidevara <mzakidevara@gmail.com>
2021-06-27 10:58:45 +0700

    cors package added

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors')
 const app = express();
 const port = 3000;
 const UserRoute = require('./routes/userRoute/UserRoute');
 const bodyParser = require('body-parser');
 
+app.use(cors())
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 app.use('/v1', UserRoute);
