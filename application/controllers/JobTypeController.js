6f0669acaf1dc25b5ef886a1dbe207ef20892154
Rhio Adjie Fabian <rhioadjie20@gmail.com>
2021-07-04 18:33:38 +0700

    Finishing API JobType

--- application/controllers/JobTypeController.js
+++ application/controllers/JobTypeController.js
@@ -1,5 +1,6 @@
 const JobType = require("../../model/JobType");
 const ResourceController = require("./ResourceController");
+const {v4: uuidv4 } = require('uuid');
 
 class JobTypeController extends ResourceController{
 
@@ -7,15 +8,56 @@ class JobTypeController extends ResourceController{
         super(JobType);
     }
 
-    async update(updatedJobType){
-        let jobTypeU = await JobType.update(updatedJobType);
-        return jobTypeU.toObject();
+    // Admin section
+    async create(jobTypeData){
+        let jobType = new JobType(uuidv4(), jobTypeData.name);
+        try{
+            let result = await jobType.save();
+            if(result){
+                return jobType.toObject();
+            } else {
+                throw new Error('Gagal membuat tipe pekerjaan');
+            }
+        } catch(e){
+            throw e;
+        }
     }
 
-    async delete(jobTypeID){
-        let result = await JobType.delete(jobTypeID);
-        return result;
+    async update(jobTypeId, updatedJobType){
+        try{
+            let jobType = await JobType.find(jobTypeId);
+            if(jobType === null) throw new Error('Tipe pekerjaan tidak ditemukan');
+    
+            try{
+                let resultUpdate = await jobType.update(updatedJobType);
+                if(resultUpdate === null) throw new Error('Tipe pekerjaan gagal diperbarui');
+
+                return resultUpdate.toObject();
+            } catch(e){
+                throw e;
+            }
+        } catch(e){
+            throw e;
+        }
     }
+
+    async delete(jobTypeId){
+        try{
+            let jobType = await JobType.find(jobTypeId);
+            if(jobType === null) throw new Error('Tipe pekerjaan tidak ditemukan');
+
+            try{
+                let result = await jobType.delete();
+                return result;
+            } catch(e){
+                throw e;
+            }
+        } catch(e){
+            throw e;
+        }
+    }
+
+   
 }
 
 module.exports = JobTypeController;
--- routes/userRouter/JobTypeRouter.js
+++ routes/userRouter/JobTypeRouter.js
@@ -21,6 +21,23 @@ router.get('/', async function(req, res) {
     }
 });
 
+// Admin
+router.post('/', async function(req, res) {
+    const jobTypeData = req.body;
+    try{
+        const jobTypeController = new JobTypeController();
+        let result = await jobTypeController.create(jobTypeData);
+
+        res.status(400);
+        res.send({ result });
+    } catch(e){
+        res.status(400);
+        let {message} = e;
+        res.send({message});
+    }
+});
+
+// User
 router.get('/:jobTypeId', async function(req, res) {
     const {jobTypeId} = req.params;
     try{
@@ -32,6 +49,38 @@ router.get('/:jobTypeId', async function(req, res) {
             message: "Berhasil",
             result
         });
+    } catch(e){
+        res.status(400);
+        let {message} = e;
+        res.send({message});
+    }
+});
+
+// Admin
+router.put('/:jobTypeId', async function(req, res) {
+    const {jobTypeId} = req.params;
+    const jobTypeData = req.body;
+    try{
+        const jobTypeController = new JobTypeController();
+        let result = await jobTypeController.update(jobTypeId, jobTypeData);
+
+        res.status(200);
+        res.send({ result });
+    } catch(e){
+        res.status(400);
+        let {message} = e;
+        res.send({message});
+    }
+});
+
+router.get('/:jobTypeId', async function(req, res) {
+    const {jobTypeId} = req.params;
+    try{
+        const jobTypeController = new JobTypeController();
+        let result = await jobTypeController.delete(jobTypeId);
+
+        res.status(200);
+        res.send({ result });
     } catch(e){
         res.status(400);
         let {message} = e;
